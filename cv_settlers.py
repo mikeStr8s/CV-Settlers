a04206aafc133ac5d90a65b9c45c557880330104
MikeStr8s <stratinm@mail.gvsu.edu>
2018-03-22 15:42:19 -0400

    Added some canny edge detection, and made a new markdown of the research notes I have gathered for those interested.

--- cv_settlers.py
+++ cv_settlers.py
@@ -3,12 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Get the original image and set to grayscale, this makes it so that there is only ever "one" color on screen
-imgOriginal = cv2.imread("catan-game-board.jpg", cv2.IMREAD_GRAYSCALE)
+catanBoardImg = cv2.imread("catan-game-board.jpg", 0)
 
-# Scale down the original image to half scale because source image is huge
-imgScaled = cv2.resize(imgOriginal, (0, 0), fx=0.5, fy=0.5)
+# Canny edge detection on catanBoardImg
+cannyBoardImg = cv2.Canny(catanBoardImg, 50, 400)
+cannyBoardImg = cv2.resize(cannyBoardImg, (0, 0), fx=0.25, fy=0.25)
+
+# Save resulting canny edge image to png file
+cv2.imwrite("smalledges.png", cannyBoardImg)
 
 # Logic to show the image and close the display window the moment ANY key is pressed
-cv2.imshow("GreyImage", imgScaled)
+cv2.imshow("Edges", cannyBoardImg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
