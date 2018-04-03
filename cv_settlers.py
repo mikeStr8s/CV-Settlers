a40032f3b9f4f279ec31fe585e84d4882991f637
MikeStr8s <stratinm@mail.gvsu.edu>
2018-04-03 14:08:56 -0400

    Added contour detection and average color detection WITHIN those contours

--- cv_settlers.py
+++ cv_settlers.py
@@ -3,57 +3,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 cap = cv2.VideoCapture(1)
+img = None
+frame = None
+cont = None
 
 while cap.isOpened():
     ret, frame = cap.read()
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, minDist=100, maxRadius=100)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cont = contours[1:]
+    frame = cv2.drawContours(frame, cont, -1, (0, 255, 0), 3)
+    # cv2.fillPoly(frame, pts=contours, color=(255, 0, 0))
+    cv2.imshow('contour detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # img = frame
+        break
 
-    cv2.line(frame, (50, 0), (50, 100), (0,255,0), 1)
+while cap.isOpened():
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            print("x: ", x, ", y: ", y)
+    final = np.zeros(frame.shape, np.uint8)
+    mask = np.zeros(gray.shape, np.uint8)
 
-    cv2.imshow('frame', frame)
+    for i in range(0, len(cont)):
+        mask[...] = 0
+        cv2.drawContours(mask, cont, i, 255, -1)
+        cv2.drawContours(frame, cont, i, cv2.mean(frame, mask), -1)
+
+    cv2.imshow('colored contours', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-# field_tile = cv2.imread("many_tiles.jpg")
-# output = field_tile.copy()
-# gray = cv2.cvtColor(field_tile, cv2.COLOR_BGR2GRAY)
-#
-# circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, minDist=500, minRadius=150, maxRadius=300)
-#
-# if circles is not None:
-#     circles = np.round(circles[0, :]).astype("int")
-#     for (x, y, r) in circles:
-#         # draw the circle in the output image, then draw a rectangle
-#         # corresponding to the center of the circle
-#         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-#         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-#
-# cv2.imwrite('those_arent_circles.jpg', output)
-#
-# cv2.namedWindow('Image')
-# cv2.imshow('Image', np.hstack([field_tile, output]))
-# cv2.resizeWindow('Image', 600, 600)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
